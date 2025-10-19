3304726b412e6df3327a94603b516514fcf937f7
maljaethereal <121117752+MaksimOlienyk@users.noreply.github.com>
2025-10-19 19:14:18 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿abstract class AbstractClass
+{
+    public void TemplateMethod()
+    {
+        Step1(); Step2();
+    }
+    protected abstract void Step1();
+    protected abstract void Step2();
+}
+
+class ConcreteClass : AbstractClass
+{
+    protected override void Step1() => Console.WriteLine("Step1");
+    protected override void Step2() => Console.WriteLine("Step2");
+}
+
+class Program { static void Main() => new ConcreteClass().TemplateMethod(); }
+
